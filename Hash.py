ea58e60725745588f66994db116df0ab3131f3f1
Nedal <44215261+CyberSy@users.noreply.github.com>
2018-10-29 17:28:54 +0200

    Create Hash.py

---
+++ Hash.py
@@ -0,0 +1,25 @@
+from hashing import hashingtxt
+import hashlib
+
+text = str(input ("Enter a text: "))
+
+while text == '':
+
+	print ("[!] Please Enter A Text To Continue! ")
+
+	text = str(input("Enter a text: "))
+
+print ("Available hash types: \n ")
+
+print (hashlib.algorithms_available)
+
+hashtype = (input("Enter a Hash Type: "))
+
+while hashtype == '':
+
+	print ("[!] Please Enter a Hash type To Continue! ")
+
+	hashtype = (input("Enter a Hash Type: "))
+
+h = hashingtxt()
+print ("[+] your Hash is: " + h.hashtext(text,hashtype))
